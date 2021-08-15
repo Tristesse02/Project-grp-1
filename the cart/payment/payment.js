f84cfb25edbbc8c93714739c316bc368a48d6c4f
Tien Minh Vu <vtminh02@gmail.com>
2021-08-15 22:08:43 +0700

    update: add folder main page

--- the cart/payment/payment.js
+++ the cart/payment/payment.js
@@ -13,21 +13,5 @@ function submit(){
     return;
 }
 
-// var c = document.getElementsByClassName("inp-sec");
-// var d = document.getElementsByClassName("expiration");
-// var e = document.getElementsByClassName("CV-code");
-// var f = document.getElementsByClassName("amount");
-// var b = document.querySelectorAll("#warn-body-content-fullName");
-// console.log(c[0].value.length);
-// console.log(d);
-// function submit(){
-//     var nameValue = c[0].value;
-//     if(nameValue.length == 0){
-//         b[i].innerHTML = "These space must be filled*";
-//         console.log(b);
-//     }
-//     return;
-// }
-// var c = document.querySelector("#checkbox");
-// console.log(c);
+
 
